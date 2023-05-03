735bdc3be6a7c6347e1b097651ebe72d6ef4bbd7
jungminmin00 <jjungjj0311@gmail.com>
2023-05-03 20:57:38 +0900

    모듈 저장

--- js/index.js
+++ js/index.js
@@ -1,11 +1,56 @@
-function nav(){     // top포함
-    $('nav ul a').on('click', function(){
-        let href = $(this).attr('href');
-        console.log(href);
-        // 누른것 높이 알아서 내려가게 하기
-        let idHeight = $(href).offset().top;
-        $('html,body').animate{scrollTop(idHeight), 300};
-        return false;   // a 막아주기
-    })
-    
+// 내비게이션
+function nav(){
+  $('#header a').on('click', function(){
+    let href = $(this).attr('href');
+    let idHeight = $(href).offset().top;
+    $('html,body').animate({scrollTop: idHeight}, 500);
+    console.log(href, idHeight);
+    return false;
+  });
+  $('aside>p').on('click', function(){
+    $('html,body').animate({scrollTop: 0}, 500);
+  });
+}
+// 모바일, 타블릿 
+function tNav(){
+  let navWidth = $('nav').width();
+  $('header #navMenu').on('click', function(){
+    $(this).css('color', '#fff');
+    $('nav').animate({marginLeft:'0'},500);
+  });
+  $('nav ul a').on('click', function(){
+    const navA = $(this).attr('href');
+    const posY = $(navA).offset().top;
+    const headerT = $('header').innerHeight();
+    $('nav').css('left', '-'+'navWidth'+'px')
+    $('html, body').animate({scrollTop: posY - headerT});
+    $('header #navMenu').css('color', '#000');
+    $('nav').css('left', '-'+navWidth+'px');
+    return false;
+  });
+  $('nav p').on('click', function(){
+    $('nav').css('left', '-'+navWidth+'px');
+    $('header #navMenu').css('color', '#000');
+  });
+}
+function mNav(){
+  let navWidth = $('nav').width();
+  $('header #navMenu').on('click', function(){
+    $(this).css('color', '#fff');
+    $('nav').animate({left:'0'},500);
+  });
+  $('nav ul a').on('click', function(){
+    const navA = $(this).attr('href');
+    const posY = $(navA).offset().top;
+    const headerT = $('header').innerHeight();
+    $('nav').css('left', '-'+'navWidth'+'px')
+    $('html, body').animate({scrollTop: posY - headerT});
+    $('header #navMenu').css('color', '#000');
+    $('nav').css('left', '-'+navWidth+'px');
+    return false;
+  });
+  $('nav p').on('click', function(){
+    $('nav').css('left', '-'+navWidth+'px');
+    $('header #navMenu').css('color', '#000');
+  });
 }
